507fa7df349b2005f3240a3be0482561788c7d7d
Issei Aoki <i.greenwood.dev@gmail.com>
2016-03-25 16:19:05 +0900

    Fix bugs

--- app/src/main/java/com/example/floatingvideo/VideoService.java
+++ app/src/main/java/com/example/floatingvideo/VideoService.java
@@ -3,6 +3,8 @@ package com.example.floatingvideo;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.hardware.display.DisplayManager;
@@ -27,25 +29,27 @@ import java.io.IOException;
 
 public class VideoService extends Service implements TextureView.SurfaceTextureListener, MediaPlayer.OnCompletionListener{
     private static final String TAG = VideoService.class.getSimpleName();
-    private static final float MIN_SCALE = 0.5f;
+    private static final float MIN_SCALE = 0.3f;
     private static final float MAX_SCALE = 2.0f;
 
-    TextView moveButton, removeButton, resizeButton;
+    TextView removeButton, resizeButton;
     TextureView videoView;
     FrameLayout container;
     LayoutInflater inflater;
-    WindowManager wm;
     WindowManager.LayoutParams params;
     MediaPlayer mediaPlayer;
+    WindowManager wm;
     DisplayManager dm;
     Display display;
     DisplayMetrics displayMetrics = new DisplayMetrics();
 
     int dispWidth, dispHeight;
+    int statusBarHeight;
     float w, h;
     float density;
     Rect dispRect = new Rect();
     float scale = 1.0f;
+    boolean attached = false;
 
     public VideoService() {
     }
@@ -57,144 +61,205 @@ public class VideoService extends Service implements TextureView.SurfaceTextureL
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        inflater = LayoutInflater.from(this);
+        Log.d(TAG, "onStartCommand");
 
         wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         dm = (DisplayManager) getSystemService(
                 Context.DISPLAY_SERVICE);
         display = wm.getDefaultDisplay();
+        final Resources resources = getResources();
+        final int statusBarHeightId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        if (statusBarHeightId > 0) {
+            statusBarHeight = resources.getDimensionPixelSize(statusBarHeightId);
+        } else {
+            statusBarHeight = 0;
+        }
         updateDefaultDisplayInfo();
         dm.registerDisplayListener(displayListener, null);
 
-        density = displayMetrics.density;
-        w = 300 * density;
-        h = 225 * density;
+        inflater = LayoutInflater.from(this);
+        container = (FrameLayout) inflater.inflate(R.layout.video_content, null);
+        videoView = (TextureView) container.findViewById(R.id.video_view);
+        removeButton = (TextView) container.findViewById(R.id.remove_button);
+        resizeButton = (TextView) container.findViewById(R.id.resize_button);
 
-        dispRect = new Rect(0, 0, dispWidth, dispHeight);
-        params = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_PHONE);
+        params = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_PRIORITY_PHONE);
         params.flags |= WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                 | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                 | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
-        params.alpha = 0.8f;
+        params.format = PixelFormat.TRANSLUCENT;
+        params.alpha = 1.0f;
         params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = constrain(dispRect.left, dispRect.right - Math.round(Math.round(w)), Math.round(dispWidth - w));
-        params.y = constrain(dispRect.top, dispRect.bottom - Math.round(h), Math.round(dispHeight - h));
-        params.width = Math.round(w);
-        params.height = Math.round(h);
-        setupVideoView();
 
-        return super.onStartCommand(intent, flags, startId);
+        setupCoordinates();
+        wm.addView(container, params);
+        attached = true;
+        videoView.setSurfaceTextureListener(this);
+        container.setOnTouchListener(moveListener);
+        removeButton.setOnTouchListener(removeListener);
+        resizeButton.setOnTouchListener(resizeListener);
+
+        return START_NOT_STICKY;
     }
 
-    private void setupVideoView() {
-        container = (FrameLayout) inflater.inflate(R.layout.video_content, null);
-        wm.addView(container, params);
-        videoView = (TextureView) container.findViewById(R.id.video_view);
-        removeButton = (TextView) container.findViewById(R.id.remove_button);
-        resizeButton = (TextView) container.findViewById(R.id.resize_button);
-        moveButton = (TextView) container.findViewById(R.id.move_button);
-        moveButton.setOnTouchListener(new View.OnTouchListener() {
-            private int initialX;
-            private int initialY;
-            private float initialTouchX;
-            private float initialTouchY;
+    private boolean updateDefaultDisplayInfo() {
+        display.getMetrics(displayMetrics);
+        dispWidth = displayMetrics.widthPixels;
+        dispHeight = displayMetrics.heightPixels;
+        return true;
+    }
 
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        initialX = params.x;
-                        initialY = params.y;
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        int diffX = (int) (event.getRawX() - initialTouchX);
-                        int diffY = (int) (event.getRawY() - initialTouchY);
-                        params.x = constrain(dispRect.left, dispRect.right - Math.round(Math.round(params.width)), initialX + diffX);
-                        params.y = constrain(dispRect.top, dispRect.bottom - Math.round(Math.round(params.height)), initialY + diffY);
-                        wm.updateViewLayout(container, params);
-                        return true;
-                    case MotionEvent.ACTION_CANCEL:
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                }
-                return false;
-            }
-        });
-        removeButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        wm.removeView(container);
-                        container = null;
-                        stopSelf();
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        return true;
-                    case MotionEvent.ACTION_CANCEL:
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                }
-                return false;
-            }
-        });
+    private void setupCoordinates() {
+        w = (w == 0) ? displayMetrics.widthPixels / MAX_SCALE : Math.min(w, displayMetrics.widthPixels / MAX_SCALE);
+        h = (h == 0) ? w * 3f / 4f : Math.min(h, w * 3f / 4f);
+        dispRect = new Rect(0, statusBarHeight, dispWidth, dispHeight);
+        params.x = constrain(dispRect.left, dispRect.right - Math.round(Math.round(w * scale)), Math.round(dispWidth - w * scale));
+        params.y = constrain(dispRect.top, dispRect.bottom - Math.round(h * scale), Math.round(dispHeight - h * scale));
+        params.width = Math.round(w * scale);
+        params.height = Math.round(h * scale);
         videoView.setPivotX(0);
         videoView.setPivotY(0);
         videoView.getLayoutParams().width = Math.round(w);
         videoView.getLayoutParams().height = Math.round(h);
-        videoView.setSurfaceTextureListener(this);
-        resizeButton.setOnTouchListener(new View.OnTouchListener() {
-            private int initialX;
-            private int initialY;
-            private float initialTouchX;
-            private float initialTouchY;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        initialX = params.x;
-                        initialY = params.y;
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        float touchX = event.getRawX();
-                        float touchY = event.getRawY();
-                        int diffX = (int) (touchX - initialTouchX);
-                        int oldW = params.width;
-                        int newW = oldW + diffX;
-                        float newScale = scale * (float) newW / (float) oldW;
-                        scale = constrain(MIN_SCALE, MAX_SCALE, newScale);
-                        Log.d(TAG, "scale = " + scale);
-                        videoView.setScaleX(scale);
-                        videoView.setScaleY(scale);
-                        params.width = Math.round(w * scale);
-                        params.height = Math.round(h * scale);
-                        wm.updateViewLayout(container, params);
-                        initialTouchX = touchX;
-                        initialTouchY = touchY;
-                        return true;
-                    case MotionEvent.ACTION_CANCEL:
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                }
-                return false;
-            }
-        });
+        videoView.setScaleX(scale);
+        videoView.setScaleY(scale);
     }
+
+    private void relayout() {
+        if (!attached) return;
+        setupCoordinates();
+        wm.updateViewLayout(container, params);
+    }
+
+    private void dismiss() {
+        if (!attached) return;
+        attached = false;
+        dm.unregisterDisplayListener(displayListener);
+        wm.removeView(container);
+    }
+
+    private final View.OnTouchListener moveListener = new View.OnTouchListener() {
+        private int initialX;
+        private int initialY;
+        private float initialTouchX;
+        private float initialTouchY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    initialX = params.x;
+                    initialY = params.y;
+                    initialTouchX = event.getRawX();
+                    initialTouchY = event.getRawY();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    int diffX = (int) (event.getRawX() - initialTouchX);
+                    int diffY = (int) (event.getRawY() - initialTouchY);
+                    params.x = constrain(dispRect.left, dispRect.right - Math.round(Math.round(params.width)), initialX + diffX);
+                    params.y = constrain(dispRect.top, dispRect.bottom - Math.round(Math.round(params.height)), initialY + diffY);
+                    wm.updateViewLayout(container, params);
+                    return true;
+                case MotionEvent.ACTION_CANCEL:
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private final View.OnTouchListener removeListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    dismiss();
+                    stopSelf();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    return true;
+                case MotionEvent.ACTION_CANCEL:
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private final View.OnTouchListener resizeListener = new View.OnTouchListener() {
+        private int initialX;
+        private int initialY;
+        private float initialTouchX;
+        private float initialTouchY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    initialX = params.x;
+                    initialY = params.y;
+                    initialTouchX = event.getRawX();
+                    initialTouchY = event.getRawY();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    float touchX = event.getRawX();
+                    float touchY = event.getRawY();
+                    int diffX = (int) (touchX - initialTouchX);
+                    int oldW = params.width;
+                    int newW = oldW + diffX;
+                    float newScale = scale * (float) newW / (float) oldW;
+                    scale = constrain(MIN_SCALE, MAX_SCALE, newScale);
+                    videoView.setScaleX(scale);
+                    videoView.setScaleY(scale);
+                    params.width = Math.round(w * scale);
+                    params.height = Math.round(h * scale);
+                    wm.updateViewLayout(container, params);
+                    initialTouchX = touchX;
+                    initialTouchY = touchY;
+                    return true;
+                case MotionEvent.ACTION_CANCEL:
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private final DisplayManager.DisplayListener displayListener =
+            new DisplayManager.DisplayListener() {
+                @Override
+                public void onDisplayAdded(int displayId) {
+                    Log.d(TAG, "onDisplayAdded");
+                }
+
+                @Override
+                public void onDisplayChanged(int displayId) {
+                    Log.d(TAG, "onDisplayChanged");
+                    if (displayId == display.getDisplayId()) {
+                        updateDefaultDisplayInfo();
+                        setupCoordinates();
+                        relayout();
+                    }
+                }
+
+                @Override
+                public void onDisplayRemoved(int displayId) {
+                    Log.d(TAG, "onDisplayRemoved");
+                    if (displayId == display.getDisplayId()) {
+                        dismiss();
+                    }
+                }
+            };
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (container != null) wm.removeView(container);
+        dismiss();
     }
 
     private int constrain(int min, int max, int value) {
@@ -211,9 +276,10 @@ public class VideoService extends Service implements TextureView.SurfaceTextureL
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.d(TAG, "onSurfaceTextureAvailable");
         Surface s = new Surface(surface);
         try {
-            mediaPlayer= new MediaPlayer();
+            mediaPlayer = new MediaPlayer();
             mediaPlayer.setDataSource(this, Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sample));
             mediaPlayer.setSurface(s);
             mediaPlayer.setOnCompletionListener(this);
@@ -233,13 +299,13 @@ public class VideoService extends Service implements TextureView.SurfaceTextureL
 
     @Override
     public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
+        Log.d(TAG, "onSurfaceTextureSizeChanged");
     }
 
     @Override
     public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
         Log.d(TAG, "onSurfaceTextureDestroyed");
-        if(mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.release();
             mediaPlayer = null;
@@ -256,44 +322,4 @@ public class VideoService extends Service implements TextureView.SurfaceTextureL
         mediaPlayer.seekTo(0);
         mediaPlayer.start();
     }
-
-    private boolean updateDefaultDisplayInfo() {
-        display.getMetrics(displayMetrics);
-        dispWidth = displayMetrics.widthPixels;
-        dispHeight = displayMetrics.heightPixels;
-        return true;
-    }
-
-    private void dismiss(){
-        dm.unregisterDisplayListener(displayListener);
-        wm.removeView(container);
-    }
-
-    private final DisplayManager.DisplayListener displayListener =
-            new DisplayManager.DisplayListener() {
-                @Override
-                public void onDisplayAdded(int displayId) {
-                    Log.d(TAG, "onDisplayAdded");
-                }
-
-                @Override
-                public void onDisplayChanged(int displayId) {
-                    Log.d(TAG, "onDisplayChanged");
-                    if (displayId == display.getDisplayId()) {
-                        if (updateDefaultDisplayInfo()) {
-
-                        }else{
-                            dismiss();
-                        }
-                    }
-                }
-
-                @Override
-                public void onDisplayRemoved(int displayId) {
-                    Log.d(TAG, "onDisplayRemoved");
-                    if (displayId == display.getDisplayId()) {
-                        dismiss();
-                    }
-                }
-            };
 }
